515723351eb055f43f5ac7653efd0e35805df588
Thapanee Chayakul <thapanee.chayakul@allianz.com>
2018-10-09 13:53:09 +0700

    Create api for get ether balance

--- src/main/java/com/example/web3jservice/controllers/AccountController.java
+++ src/main/java/com/example/web3jservice/controllers/AccountController.java
@@ -5,6 +5,20 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.web3j.crypto.CipherException;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.WalletUtils;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.response.EthAccounts;
+import org.web3j.protocol.core.methods.response.EthGetBalance;
+import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+import org.web3j.protocol.http.HttpService;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.concurrent.ExecutionException;
 
 @Api
 @RestController
@@ -12,7 +26,10 @@ public class AccountController {
 
     @PostMapping("/accounts/balance")
     @ApiOperation(value = "Get Ether Balance for a single Address")
-    public ResponseEntity<String> getAddress(String address){
-        return ResponseEntity.ok(address);
+    public ResponseEntity<String> getEtherBalance(String address) throws ExecutionException, InterruptedException {
+        Web3j web3j = Web3j.build(new HttpService("https://mainnet.infura.io/v3/e4e6510f11544ac1aaa067eeff315655"));
+        EthGetBalance ethGetBalance = web3j.ethGetBalance(address, DefaultBlockParameterName.LATEST).sendAsync().get();
+        BigInteger nonce = ethGetBalance.getBalance();
+        return ResponseEntity.ok(String.valueOf(nonce));
     }
 }
